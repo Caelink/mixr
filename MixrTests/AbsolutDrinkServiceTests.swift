aebb9cb3dbcec696828bfae6e24f9d3ad9741ab1
Caelin Jackson-King <caelink@gmail.com>
2017-05-30 22:12:39 -0400

    Created version of new stuff without Pods junk

--- MixrTests/AbsolutDrinkServiceTests.swift
+++ MixrTests/AbsolutDrinkServiceTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import RxTest
 @testable import Mixr
 
 class AbsolutDrinkServiceTests: XCTestCase {
@@ -17,9 +18,10 @@ class AbsolutDrinkServiceTests: XCTestCase {
         partsPerIngredient:[Ingredient:Int](),
         extraInformation:"")]
     
+    let disposeBag = DisposeBag()
+    
     override func setUp() {
         super.setUp()
-        
     }
     
     override func tearDown() {
@@ -28,19 +30,19 @@ class AbsolutDrinkServiceTests: XCTestCase {
     }
     
     func testValidDrink() {
-        let asyncExpectation = expectation(description: "networkTest")
-        var callData: [DrinkModel]
-        callData = []
-        
-        subject.searchByName(name: "") { (data) in
-            asyncExpectation.fulfill()
-            callData.append(contentsOf: data)
-        }
-        
-        self.waitForExpectations(timeout: 100.0) { (error) in
-            XCTAssertNil(error, "Call didn't finish in time")
-            XCTAssert(!callData.isEmpty, "no data was returned")
-        }
+//        let asyncExpectation = expectation(description: "networkTest")
+//        var callData: [DrinkModel]
+//        callData = []
+//        
+//        subject.search("") { (data) in
+//            asyncExpectation.fulfill()
+//            callData.append(contentsOf: data)
+//        }
+//        
+//        self.waitForExpectations(timeout: 100.0) { (error) in
+//            XCTAssertNil(error, "Call didn't finish in time")
+//            XCTAssert(!callData.isEmpty, "no data was returned")
+//        }
     }
     
     
@@ -49,10 +51,15 @@ class AbsolutDrinkServiceTests: XCTestCase {
         var callData: [DrinkModel]
         callData = []
 
-        subject.searchByName(name: "lol what am I doing?/") { (data) in
-            asyncExpectation.fulfill()
-            callData.append(contentsOf: data)
-        }
+        subject.knownDrinks
+            .subscribe(onNext: { (data) in
+                asyncExpectation.fulfill()
+//                callData = callData + data
+            })
+            .addDisposableTo(disposeBag)
+        
+        subject.search(by: "lol what am I doing?/")
+        
         
         self.waitForExpectations(timeout: 100.0) { (error) in
             XCTAssertNil(error, "Call didn't finish in time")
